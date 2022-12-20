186ed6b2d5922afd4473f636b47e7ea7ac93b70c
Yuriy-Burilov <yuriy.burilov@umbrellait.com>
2022-12-20 17:32:59 +0400

    index page add description

--- src/Pages/Index/Index.page.tsx
+++ src/Pages/Index/Index.page.tsx
@@ -10,16 +10,31 @@ export const IndexPage: FC = () => {
         <Link to={"/films"}>
           <Button>Films</Button>
         </Link>
+        <p>Практика с react-query на реальном api (swapi) - prefetch</p>
       </li>
       <li>
         <Link to={"/tasks"}>
           <Button>Tasks</Button>
         </Link>
+        <p>
+          Практика с react-query (mock) - prefetch, pagination, cache control
+        </p>
       </li>
       <li>
         <Link to={"/alt-tasks"}>
           <Button>Tasks - infinity loading</Button>
         </Link>
+        <p>Практика с react-query (mock) - infinity scroll</p>
+        <p>
+          Данный подход лучше всего подходит для отображения неизменяемых
+          списков
+        </p>
+
+        <p>
+          в данном случе был использован плохой подход в целях обучения, для
+          лучшей реализации, необходимо использовать кастомный компонент, на
+          основе пагинации, который будет обновлять только видимые страницы
+        </p>
       </li>
     </StyledIndexMenu>
   );
