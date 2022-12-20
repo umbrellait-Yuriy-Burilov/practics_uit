c885a702dfcd6a86ffd0647ee63cf93f4b6c95dd
Yuriy-Burilov <yuriy.burilov@umbrellait.com>
2022-12-20 17:32:56 +0400

    update Film.page.tsx filmId from useParam

--- src/Pages/Films/Film.page.tsx
+++ src/Pages/Films/Film.page.tsx
@@ -1,19 +1,26 @@
-import {FC} from "react";
-import {useGetFilm} from '../../hooks/api/films.api.hooks';
-import {Loading} from '../../Components/_UI/Loading/Loading';
+import { FC } from "react";
+import { useGetFilm } from "../../hooks/api/films.api.hooks";
+import { Loading } from "../../Components/_UI/Loading/Loading";
+import { Update } from "../../Components/_UI/Update/Update";
+import {useParams} from 'react-router-dom';
 
-export const FilmPage: FC<{filmUrl: string}> = ({filmUrl}) => {
-  const {data: film, isLoading} = useGetFilm(filmUrl);
+export const FilmPage: FC = () => {
+  const { filmId } = useParams();
+
+  const { data: film, isLoading, isFetching } = useGetFilm(filmId as string);
 
   if (isLoading) {
-    return <Loading />
+    return <Loading />;
   }
 
   if (film) {
-    return <div>
-      <h3>{film.title}</h3>
-      <p>{film.opening_crawl}</p>
-    </div>
+    return (
+      <div>
+        <h3>{film.title}</h3>
+        <p>{film.opening_crawl}</p>
+        <Update isUpdate={isFetching} />
+      </div>
+    );
   }
   return null;
 };
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
-import { FilmsPage } from "../Pages/Films/Films.page";
+import { Films } from "../Components/Films/Films";
 import {NotFoundPage} from '../Pages/NotFoundPage/NotFound.page';
+import {FilmPage} from '../Pages/Films/Film.page';
 
 export const Router: FC = () => {
   return (
     <Routes>
-      <Route path={"/"} element={<FilmsPage />} />
+      <Route path={"/"} element={<Films />} />
+      <Route path={'/film/:filmId'} element={<FilmPage />} />
       <Route path={"*"} element={<NotFoundPage />} />
     </Routes>
   );
