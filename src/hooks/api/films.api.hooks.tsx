a438981fee9b0c4c5aac07a75578a029f88068d0
Yuriy-Burilov <yuriy.burilov@umbrellait.com>
2022-12-20 17:32:56 +0400

    update films.api.hooks.tsx add support query search

--- src/hooks/api/films.api.hooks.tsx
+++ src/hooks/api/films.api.hooks.tsx
@@ -1,26 +1,39 @@
-import {useQuery} from 'react-query';
-import {API_FILMS_URL} from './config.api';
-import axios from 'axios';
+import { useQuery } from "react-query";
+import { API_FILMS_URL } from "./config.api";
+import axios from "axios";
 
-type FilmType = {
-  title: string,
-  url: string,
-  created: string,
-  director: string,
-  episode_id: string,
-  planets: [],
-  characters: [],
-  producer: string,
-  release_date: string,
+export type FilmType = {
+  title: string;
+  url: string;
+  created: string;
+  director: string;
+  episode_id: string;
+  planets: [];
+  characters: [];
+  producer: string;
+  release_date: string;
 };
-type FilmsType = FilmType[];
-type FilmsResponseType = {
-  count: number,
-  results: FilmsType
-}
+export type FilmsType = FilmType[];
 
-export const useGetFilms = () => useQuery(
-  ['films'],
-  () => axios.get<FilmsResponseType>(API_FILMS_URL).then(res => res.data),
-  {}
-)
+type FilmsResponseType = {
+  count: number;
+  results: FilmsType;
+};
+
+export const useGetFilms = (query?: string) => {
+  let url = API_FILMS_URL;
+  const queryKey = ["films"];
+
+  if (query !== '' && query) {
+    queryKey.push(query);
+    url += `?search=${query}`
+  }
+
+  return useQuery(
+    queryKey,
+    () => axios.get<FilmsResponseType>(url).then((res) => res.data),
+    {
+      enabled: query !== ''
+    }
+  );
+}
