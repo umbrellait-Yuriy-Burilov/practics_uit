9616c946fed4538acd67daba3f94b6bf28e8d0f2
Yuriy-Burilov <yuriy.burilov@umbrellait.com>
2022-12-20 17:32:56 +0400

    QueryProvider.tsx export queryClient

--- src/config/QueryProvider.tsx
+++ src/config/QueryProvider.tsx
@@ -2,7 +2,7 @@ import {QueryClient, QueryClientProvider} from 'react-query';
 import React, {FC, PropsWithChildren} from 'react';
 import {ReactQueryDevtools} from 'react-query/devtools';
 
-const queryClient = new QueryClient({
+export const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
         refetchOnWindowFocus: false,
