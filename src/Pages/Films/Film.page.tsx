66f992d34d02904fac926f14c033a8acaa05d34a
Yuriy-Burilov <yuriy.burilov@umbrellait.com>
2022-12-20 17:32:56 +0400

    Film.page.tsx add ShowError.txt Component

--- src/Pages/Films/Film.page.tsx
+++ src/Pages/Films/Film.page.tsx
@@ -3,24 +3,28 @@ import { useGetFilm } from "../../hooks/api/films.api.hooks";
 import { Loading } from "../../Components/_UI/Loading/Loading";
 import { Update } from "../../Components/_UI/Update/Update";
 import {useParams} from 'react-router-dom';
+import {ShowError} from '../../Components/_UI/ShowError/ShowError';
 
 export const FilmPage: FC = () => {
   const { filmId } = useParams();
 
-  const { data: film, isLoading, isFetching } = useGetFilm(filmId as string);
+  const { data: film, isLoading, isFetching, isError, error } = useGetFilm(filmId as string);
 
   if (isLoading) {
     return <Loading />;
   }
 
-  if (film) {
-    return (
-      <div>
-        <h3>{film.title}</h3>
-        <p>{film.opening_crawl}</p>
-        <Update isUpdate={isFetching} />
-      </div>
-    );
-  }
-  return null;
+  return (
+    <div>
+      <ShowError isError={isError} error={error} />
+      {film && (
+        <>
+          <h3>{film.title}</h3>
+          <p>{film.opening_crawl}</p>
+          <Update isUpdate={isFetching} />
+        </>
+      )}
+
+    </div>
+  );
 };
