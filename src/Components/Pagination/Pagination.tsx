6742b234e3405f9973cb38874e6cb8a80427b8a7
Yuriy-Burilov <yuriy.burilov@umbrellait.com>
2022-12-20 17:32:59 +0400

    pagination update add first and last page

--- src/Components/Pagination/Pagination.tsx
+++ src/Components/Pagination/Pagination.tsx
@@ -1,30 +1,55 @@
 import { FC, memo } from "react";
 import { Link } from "react-router-dom";
+import Button from "../_UI/Button/Button";
+import styled from "styled-components";
 
+const StyledPagination = styled.p`
+  display: grid;
+  grid-auto-flow: column;
+  justify-content: start;
+  align-items: center;
+  gap: 0 8px;
+`;
 export const Pagination: FC<{
   path: string;
   currentPage: number;
   pageCount: number;
 }> = memo(({ path, currentPage, pageCount }) => {
   return (
-    <p>
+    <StyledPagination>
+      {currentPage >= 2 ? (
+        <Link to={`${path}/${1}`}>
+          <Button>первая</Button>
+        </Link>
+      ) : (
+        <Button disabled>первая</Button>
+      )}
+
       {currentPage <= 1 ? (
-        <button disabled={true}>предыдущая</button>
+        <Button disabled={true}>предыдущая</Button>
       ) : (
         <Link to={currentPage === 2 ? `${path}` : `${path}/${currentPage - 1}`}>
-          <button>предыдущая</button>
+          <Button>предыдущая</Button>
         </Link>
       )}
 
       {` ${currentPage} `}
 
       {currentPage >= pageCount ? (
-        <button disabled={true}>следующая</button>
+        <Button disabled={true}>следующая</Button>
       ) : (
         <Link to={`${path}/${Number(currentPage) + 1}`}>
-          <button>следующая</button>
+          <Button>следующая</Button>
         </Link>
       )}
-    </p>
+
+      {currentPage <= pageCount - 1 ? (
+        <Link to={`${path}/${pageCount}`}>
+          <Button>последняя</Button>
+        </Link>
+      ) : (
+        <Button disabled>последняя</Button>
+      )}
+    </StyledPagination>
   );
 });
