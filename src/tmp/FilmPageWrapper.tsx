22ec42a1886276fcf06aa1d6a039afc2045ad7f1
Yuriy-Burilov <yuriy.burilov@umbrellait.com>
2022-12-20 17:32:57 +0400

    add query prefetch for FilmPageWrapper.tsx

--- src/tmp/FilmPageWrapper.tsx
+++ src/tmp/FilmPageWrapper.tsx
@@ -1,11 +1,29 @@
-import {FC, useState} from 'react';
-import {FilmPage} from '../Pages/Films/Film.page';
+import { FC, useEffect, useState } from "react";
+import { FilmPage } from "../Pages/Films/Film.page";
+import { queryClient } from "../config/QueryProvider";
+import { apiGetFilm } from "../hooks/api/films.api.hooks";
+import { useParams } from "react-router-dom";
 
 export const FilmPageWrapper: FC = () => {
-  const [isShow, setIsShow] = useState(true);
+  const [isShow, setIsShow] = useState(false);
+  const { filmId } = useParams();
 
-  return (<>
-    <button onClick={() => setIsShow(!isShow)}>{isShow ? 'hide': 'show'}</button>
-    {isShow ? <FilmPage /> : null}
-  </>)
-}
+  useEffect(() => {
+    queryClient?.prefetchQuery(["film", filmId], () => apiGetFilm(filmId as string));
+  }, [filmId]);
+
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+        aliquam amet animi corporis doloribus, eligendi ex exercitationem
+        facilis id ipsam natus necessitatibus optio, placeat quas suscipit
+        totam, velit voluptatibus! Culpa!
+      </p>
+      <button onClick={() => setIsShow(!isShow)}>
+        {isShow ? "hide" : "show"}
+      </button>
+      {isShow ? <FilmPage /> : null}
+    </>
+  );
+};
