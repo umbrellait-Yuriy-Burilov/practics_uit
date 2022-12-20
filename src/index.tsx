15fc74bdc0351c63d0ee1834355ccfb946c38d1e
Yuriy-Burilov <yuriy.burilov@umbrellait.com>
2022-12-20 17:32:55 +0400

    init-project

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
+ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
-root.render(
+).render(
   <React.StrictMode>
-    <App />
+    <div>app</div>
   </React.StrictMode>
 );
 
