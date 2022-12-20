76962fabe1bb878fd406c95e7d7c613e53885b8f
Yuriy-Burilov <yuriy.burilov@umbrellait.com>
2022-12-20 17:32:56 +0400

    include SearchFilm Component in Films.tsx

--- src/Components/Films/Films.tsx
+++ src/Components/Films/Films.tsx
@@ -1,13 +1,16 @@
-import {FC} from "react";
-import {useGetFilms} from "../../hooks/api/films.api.hooks";
-import {Loading} from '../_UI/Loading/Loading';
-import {ShowError} from '../_UI/ShowError/ShowError';
-import {Update} from '../_UI/Update/Update';
+import { FC, useState } from "react";
+import { useGetFilms } from "../../hooks/api/films.api.hooks";
+import { Loading } from "../_UI/Loading/Loading";
+import { ShowError } from "../_UI/ShowError/ShowError";
+import { Update } from "../_UI/Update/Update";
+import { FilmList } from "../FilmList/FilmList";
+import { SearchFilm } from "../SearchFilm/SearchFilm";
 
 export const Films: FC = () => {
-  const {data, isLoading, isError, isFetching, error} = useGetFilms();
+  const { data, isLoading, isError, isFetching, error } = useGetFilms();
+  const [searchFilm, setSearchFilm] = useState("");
 
-  const films = data?.results;
+  const films = data?.results ?? [];
 
   if (isLoading) {
     return <Loading />;
@@ -15,10 +18,20 @@ export const Films: FC = () => {
 
   return (
     <div>
+      <p>
+        Search film:{" "}
+        <input
+          type="text"
+          value={searchFilm}
+          onChange={(e) => setSearchFilm(e.target.value)}
+        />
+      </p>
+
+      <SearchFilm search={searchFilm} />
+
       <Update isUpdate={isFetching} />
       <ShowError isError={isError} error={error} />
-      {films &&
-        films.map((film) => <div key={film.episode_id}>{film.title}</div>)}
+      <FilmList films={films} />
     </div>
   );
 };
