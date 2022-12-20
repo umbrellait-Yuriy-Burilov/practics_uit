5130abc17ac696e4dfccb137678efac4f14c4505
Yuriy-Burilov <yuriy.burilov@umbrellait.com>
2022-12-20 17:32:58 +0400

    storybook update global config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,28 @@
+import { BrowserRouter } from "react-router-dom";
+
+// global params
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
+    expanded: true,
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+// global args
+export const argTypes = {
+  theme: { control: "select", options: ["light", "dark"] },
+};
+export const args = { theme: "light" };
+
+// decorators
+export const decorators = [
+  (Story, Context) => (
+    <BrowserRouter>
+      <Story />
+    </BrowserRouter>
+  ),
+];
