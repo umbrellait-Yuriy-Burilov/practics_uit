9d11802fbdadf04edfba429593dc04027758eeb4
Yuriy-Burilov <yuriy.burilov@umbrellait.com>
2022-12-20 17:32:56 +0400

    include Planet component in FilmListItem.tsx

--- src/Components/FilmList/FilmListItem.tsx
+++ src/Components/FilmList/FilmListItem.tsx
@@ -1,6 +1,18 @@
-import React, {FC} from 'react';
-import {FilmType} from '../../hooks/api/films.api.hooks';
+import React, { FC } from "react";
+import { FilmType } from "../../hooks/api/films.api.hooks";
+import { Planet } from "../Planet/Planet";
 
-export const FilmListItem: FC<{film: FilmType}> = ({film}) => {
-  return <div>{film.title}</div>
-}
+export const FilmListItem: FC<{ film: FilmType }> = ({ film }) => {
+  return (
+    <div>
+      <h5>{film.title}</h5>
+      <ul>
+        {film.planets.map((planetUrl) => (
+          <li key={planetUrl}>
+            <Planet planetUrl={planetUrl} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
