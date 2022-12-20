5a7eb265410ab0b022111a00c950573290558d41
Yuriy-Burilov <yuriy.burilov@umbrellait.com>
2022-12-20 17:32:56 +0400

    include Films Component in App

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,5 +1,10 @@
-import {FC} from 'react';
+import { FC } from "react";
+import { Films } from "../Films/Films";
 
-export const App:FC = () => {
-  return <div>app</div>
-}
+export const App: FC = () => {
+  return (
+    <div>
+      <Films />
+    </div>
+  );
+};
