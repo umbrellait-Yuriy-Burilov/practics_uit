f45b093174774e54fc83407e0809aa140110a8b1
Yuriy-Burilov <yuriy.burilov@umbrellait.com>
2022-12-20 17:32:56 +0400

    Films Component include Update and ShowError

--- src/Components/Films/Films.tsx
+++ src/Components/Films/Films.tsx
@@ -2,6 +2,7 @@ import {FC} from "react";
 import {useGetFilms} from "../../hooks/api/films.api.hooks";
 import {Loading} from '../_UI/Loading/Loading';
 import {ShowError} from '../_UI/ShowError/ShowError';
+import {Update} from '../_UI/Update/Update';
 
 export const Films: FC = () => {
   const {data, isLoading, isError, isFetching, error} = useGetFilms();
@@ -14,8 +15,8 @@ export const Films: FC = () => {
 
   return (
     <div>
-      {isFetching && <div>Update...</div>}
-      {isError && <ShowError error={error} />}
+      <Update isUpdate={isFetching} />
+      <ShowError isError={isError} error={error} />
       {films &&
         films.map((film) => <div key={film.episode_id}>{film.title}</div>)}
     </div>
