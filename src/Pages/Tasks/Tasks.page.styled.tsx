4955d37228679696c46de464483c3f84bef2abf4
Yuriy-Burilov <yuriy.burilov@umbrellait.com>
2022-12-20 17:32:59 +0400

    tasks pages update render for new TaskList

---
+++ src/Pages/Tasks/Tasks.page.styled.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const TaskHeader = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: 0 16px;
+  margin: 20px 0;
+`;
--- src/Pages/Tasks/Tasks.page.tsx
+++ src/Pages/Tasks/Tasks.page.tsx
@@ -1,25 +1,73 @@
-import { FC } from "react";
-import { TasksList } from "../../Components/TasksList/TasksList";
-import { TaskType, usePostTask } from "../../hooks/api/tasks.api.hooks";
-import { TaskForm } from "../../Components/TaskForm/Task.form";
+import { FC, useCallback, useMemo } from "react";
+import { useGetTasks, usePutTask } from "../../hooks/api/tasks.api.hooks";
 import { ShowError } from "../../Components/_UI/ShowError/ShowError";
+import { TaskType } from "../../models/task.type";
+import { TaskList } from "../../Components/TaskList/TaskList";
+import { Link, useParams } from "react-router-dom";
+import Button from "../../Components/_UI/Button/Button";
+import { Loading } from "../../Components/_UI/Loading/Loading";
+import { Pagination } from "../../Components/Pagination/Pagination";
+import { Update } from "../../Components/_UI/Update/Update";
+import { AddTask } from "../../Components/AddTask/AddTask";
+import { TaskHeader } from "./Tasks.page.styled";
 
 export const TasksPage: FC = () => {
-  const { mutate, isLoading, isError, error } = usePostTask();
+  const { page: queryPage = "1" } = useParams();
 
-  const onFormSubmit = (data: Omit<TaskType, "id">) => {
-    mutate(data);
-  };
+  const {
+    data: { tasks = [], count = 0 } = {},
+    isLoading,
+    isFetching,
+    isError,
+    error,
+  } = useGetTasks(queryPage);
+
+  const { mutate } = usePutTask();
+
+  const pageCount = count ? Math.ceil(count / 10) : 0;
+
+  const tasksMemo = useMemo(() => tasks, [tasks]);
+
+  const afterRender = useCallback(
+    (task: TaskType) => (
+      <Link to={`/task/${task.id}`}>
+        <Button size={"small"}>edit</Button>
+      </Link>
+    ),
+    []
+  );
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <>
-      <TasksList />
+      <TaskHeader>
+        <Link to={"/"}>
+          <Button>{`<`} Home</Button>
+        </Link>
+
+        <div>
+          <Update isUpdate={isFetching} />
+        </div>
+      </TaskHeader>
+
       <ShowError isError={isError} error={error} />
-      <TaskForm
-        onSubmit={onFormSubmit}
-        isLoading={isLoading}
-        submitText={isLoading ? "loading" : "save"}
+
+      <TaskList
+        tasks={tasksMemo}
+        after={afterRender}
+        onTaskUpdate={(task: TaskType) => mutate(task)}
       />
+
+      <Pagination
+        path={"/tasks"}
+        currentPage={Number(queryPage)}
+        pageCount={pageCount}
+      />
+
+      <AddTask />
     </>
   );
 };
