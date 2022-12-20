94efcb30ea44d58b5bf3e77b3572d147b39dd7b5
Yuriy-Burilov <yuriy.burilov@umbrellait.com>
2022-12-20 17:32:56 +0400

    include react query

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import {App} from './Components/App/App';
+import {QueryClient, QueryClientProvider} from 'react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <QueryClientProvider client={queryClient} >
+      <App />
+    </QueryClientProvider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
